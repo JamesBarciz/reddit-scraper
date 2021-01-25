efd09d2f1ea77aca9dafb95f60b8650f32cf93e7
James Barciz <jamesbarciz@jamess-mbp.lan>
2021-01-25 10:23:15 -0500

    connect to Reddit API

--- src/oauth_get.py
+++ src/oauth_get.py
@@ -1,52 +1,49 @@
-# Built-in Python modules
-import os
-
 # Standard Packages
+import requests
 import pandas as pd
 import datetime as dt
+from decouple import config
 
-# Other Packages
+# External packages
 import praw
 
 
 # Bring in the environment variables
-CLIENT_ID = os.getenv('CLIENT_ID')
-SECRET_TOKEN = os.getenv('SECRET_TOKEN')
-USER_APP_NAME = os.getenv('USER_APP_NAME')
-AUTH_USERNAME = os.getenv('AUTH_USERNAME')
-AUTH_PASSWORD = os.getenv('AUTH_PASSWORD')
+CLIENT_ID = config('CLIENT_ID')
+SECRET_TOKEN = config('SECRET_TOKEN')
+USER_APP_NAME = config('USER_APP_NAME')
+AUTH_USERNAME = config('AUTH_USERNAME')
+AUTH_PASSWORD = config('AUTH_PASSWORD')
 
-# Create an instance of Reddit
-REDDIT = praw.Reddit(client_id=CLIENT_ID,
-                     client_secret=SECRET_TOKEN,
-                     user_agent=USER_APP_NAME,
-                     username=AUTH_USERNAME,
-                     password=AUTH_PASSWORD
+REDDIT = praw.Reddit(
+  client_id=CLIENT_ID,
+  client_secret=SECRET_TOKEN,
+  password=AUTH_PASSWORD,
+  user_agent=USER_APP_NAME,
+  username=AUTH_USERNAME
 )
 
-print(REDDIT.user.me())
-# subreddit = REDDIT.subreddit('programming')
+assert REDDIT.user.me() == AUTH_USERNAME
 
-# top_subreddit = subreddit.top(limit=1)
+subreddit = REDDIT.subreddit('programming')
+top_subreddit = subreddit.top(limit=20)
 
-# topics_dict = {
-#     'title': [],
-#     'score': [],
-#     'id': [],
-#     'url': [],
-#     'created': [],
-#     'body': []
-# }
+programming = {
+  'title': [],
+  'id': [],
+  'score': [],
+  'body': [],
+  'url': []
+}
 
-# for submission in top_subreddit:
-#     topics_dict['title'].append(submission.title)
-#     topics_dict['score'].append(submission.score)
-#     topics_dict['id'].append(submission.id)
-#     topics_dict['url'].append(submission.url)
-#     topics_dict['created'].append(submission.created)
-#     topics_dict['body'].append(submission.selftext)
+for submission in top_subreddit:
+  programming['title'].append(submission.title)
+  programming['id'].append(submission.id)
+  programming['score'].append(submission.score)
+  programming['body'].append(submission.selftext)
+  programming['url'].append(submission.url)
 
-# print(topics_dict)
+print(programming.items())
 
 # auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_TOKEN)
 
@@ -60,14 +57,19 @@ print(REDDIT.user.me())
 #     'User-Agent': 'LeScraper/0.0.1'
 # }
 
-# res = requests.post('https://www.twitter.com/api/v1/access_token',
-#                     auth=auth, data=data, headers=headers)
+# res = requests.post('https://www.reddit.com/api/v1/access_token',
+#                     auth=auth, data=data, headers=headers
+# )
 
 # TOKEN = res.json()['access_token']
 
 # headers = {**headers, **{'Authorization': f'bearer {TOKEN}'}}
 
-# requests.get('https://oauth.reddit.com/api/v1/me', header=headers)
+# requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
 
 
-# #--------------------------------------
+# res = requests.get("https://oauth.reddit.com/r/programming/hot",
+#                    headers=headers
+# )
+
+# print(res.json())

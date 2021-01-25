9177f60e4eee3d074340a2ef3e492fb9bde78fbe
James Barciz <jamesjbarciz@gmail.com>
2021-01-25 16:48:05 -0500

    Continue developing scraper

---
+++ src/scraping.py
@@ -0,0 +1,132 @@
+# Standard packages
+import pandas as pd
+import pprint
+from decouple import config
+import time
+
+# External packages
+import praw
+
+
+# Bring in the environment variables
+CLIENT_ID = config('CLIENT_ID')
+SECRET_TOKEN = config('SECRET_TOKEN')
+USER_APP_NAME = config('USER_APP_NAME')
+AUTH_USERNAME = config('AUTH_USERNAME')
+AUTH_PASSWORD = config('AUTH_PASSWORD')
+
+# Instance of Reddit
+REDDIT = praw.Reddit(
+  client_id=CLIENT_ID,
+  client_secret=SECRET_TOKEN,
+  password=AUTH_PASSWORD,
+  user_agent=USER_APP_NAME,
+  username=AUTH_USERNAME
+)
+
+# Will cause the script to fail if OAuthError
+assert REDDIT.user.me() == AUTH_USERNAME
+
+# List of subrettits to gather from REDDIT API
+TEMP_SUBREDDITS = [
+  'ada', 'APLJK', 'asm', 'ATS', 'awk', 'c_programming', 'clojure', 'cobol',
+  'coffeescript', 'coldfusion', 'coq', 'cpp', 'cpp_questions', 'csharp',
+  'd_language', 'delphi', 'erlang', 'forth', 'fortran', 'fsharp', 'golang',
+  'gpgpu', 'groovy', 'haskell', 'Ioke', 'IoLanguage', 'java', 'javahelp',
+  'javascript', 'LearnJavaScript', 'latex', 'lisp', 'lua', 'matlab', 'oberon',
+  'objectivec', 'ocaml', 'pascal', 'perl', 'php', 'PostScript', 'prolog',
+  'python', 'learnpython', 'racket', 'Rlanguage', 'rstats', 'ruby', 'rust',
+  'scala', 'scheme', 'shell', 'smalltalk', 'sql', 'swift', 'tcl', 'visualbasic'
+]
+
+# Allows output to be printed in a less-cumbersome view
+pp = pprint.PrettyPrinter(indent=2)
+
+# Will store the subreddits to be displayed
+output = []
+
+
+def get_top_subreddits(subreddits, time_filter='day', limit=25):
+  """
+  get_top_subreddits(subreddits: [str, List], time_filter='day': str, limit=25: int)
+
+  Returns JSON of list of subreddits which include various attributes.
+  
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  Parameters:
+    
+    subreddits - A list of subreddits to obtain.  If a string, it will be converted into an iterable.
+
+    time_filter - ['all', 'day', 'hour', month', 'week', year'] - (default: 'day')
+
+    limit - Limits the amount of subreddits obtained
+
+  """
+
+  if isinstance(subreddits, str):
+    subreddits = [subreddits]
+
+  for r in subreddits:
+
+    subreddit = REDDIT.subreddit(r)
+    top_subreddit = subreddit.top(time_filter=time_filter, limit=limit)
+
+    for submission in top_subreddit:  # https://praw.readthedocs.io/en/latest/code_overview/models/submission.html
+
+      field_dict = {
+        'subreddit': None,
+        'title': None,
+        'id': None,
+        'score': None,
+        'link_url': None,
+        'post_url': None,
+        'body': None
+      }
+
+      field_dict['subreddit'] = r
+      field_dict['title'] = submission.title
+      field_dict['id'] = submission.id
+      field_dict['score'] = submission.score
+      field_dict['link_url'] = submission.url
+      field_dict['post_url'] = 'https://www.reddit.com' + submission.permalink
+      if submission.is_self:
+        field_dict['body'] = submission.selftext
+      else:
+        field_dict['body'] = 'https://www.reddit.com' + submission.permalink
+    
+      output.append(field_dict)
+    
+    time.sleep(3)
+
+get_top_subreddits(TEMP_SUBREDDITS)
+
+for entry in output:
+  pp.pprint(entry)
+
+
+# [{}, {}, {}]
+
+
+
+# DICT_TYPE1 = {
+#   'metadata': {
+#     {
+#       0: {
+#         'subreddit': 'something',
+#         'title': 'something',
+#         'id': 'something',
+#         'score': 'something',
+#         'link_url': 'something',
+#         'post_url': 'something',
+#         'body': 'something'
+#       },
+#       1: {
+#         'subreddit': 'something',
+#         'title': 'something',
+#         'id': 'something',
+#         'score': 'something',
+#         'link_url': 'something',
+#         'post_url': 'something',
+#         'body': 'something'
+# }}}}
